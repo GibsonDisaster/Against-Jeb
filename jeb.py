660d0060976b3df6fbd1e01bb28e25de8e2b514e
HenningTonko <henninghtonko@gmail.com>
2017-02-24 17:58:56 -0600

    Added a tkinter GUI for the bot

---
+++ jeb.py
@@ -0,0 +1,37 @@
+import tweepy
+import random
+from tkinter import *
+
+def make_tweet():
+	ag = open('againstme.txt', 'r')
+	jb = open('JebBush.txt', 'r')
+	ag_read = ag.read()
+	jb_read = jb.read()
+	ag_read = ag_read.split('\n')
+	jb_read = jb_read.split('\n')
+	ag_tweet = random.choice(ag_read)
+	jb_tweet = random.choice(jb_read)
+
+	tweet.config(text = ag_tweet + ' / ' + jb_tweet)
+
+def send_tweet():
+	auth = tweepy.OAuthHandler('g50EQnY1xcE2dnqfjrpPP4LM9', 'D6T4Qg7gkpxo1AKvvqxzblivUwUhDKQ6rxXSHKbBewmVrjqdlL')
+	auth.set_access_token('825470930688364550-P9EOzcwAJnJoL9tpEZ4VzthTgOBjJ82', 'xJjXSMtX8zHI0kV7D5em4MOn8q7xXwMCe0YVt95FfipU3')
+	api = tweepy.API(auth)
+	status = tweet.cget('text')
+	api.update_status(status)
+
+root = Tk()
+
+root.title('Against Jeb!')
+
+gbutton = Button(root, text = 'Generate Quote', command = make_tweet)
+gbutton.pack()
+
+tweet = Label(root)
+tweet.pack()
+
+sbutton = Button(root, text = 'Send Tweet', command = send_tweet)
+sbutton.pack()
+
+root.mainloop()
